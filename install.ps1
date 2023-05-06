a4b74e7d8eb186bdf8af4e38426b2b1f7349b383
Joel Maximilian Mai <joel@maispace.de>
2023-05-06 17:24:13 +0200

    [FEATURE] Add wsl installation

--- install.ps1
+++ install.ps1
@@ -4,6 +4,7 @@ winget upgrade --all
 # Install packages with winget
 $packages = @(
     "Git.Git",
+    "Docker.DockerDesktop"
     "Microsoft.PowerToys",
     "JackieLiu.NotepadsApp",
     "ModernFlyouts.ModernFlyouts",
@@ -38,8 +39,13 @@ foreach ($package in $packages) {
     }
 }
 
-# Download and install MJML
-# https://github.com/mjmlio/mjml-app/releases/download/v3.0.4/mjml-app-3.0.4-win.exe
+# Download and Install MJML
+$url = 'https://github.com/mjmlio/mjml-app/releases/download/v3.0.4/mjml-app-3.0.4-win.exe'
+$output = "$env:TEMP\mjml-app-3.0.4-win.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Start-Process -FilePath $output -ArgumentList '/S' -Wait
+
 
 # Download and install Starship for cmd
 $starshipLuaPath = "$env:LOCALAPPDATA\clink\starship.lua"
@@ -56,14 +62,36 @@ try {
 } finally {
 }
 
+# Set default WSL version to 2
+Write-Host "Setting default WSL version to 2..."
+wsl --set-default-version 2
 
-# WSL install from https://aka.ms/wslubuntu2204 manually
-# wsl --list --online
-# wsl --set-default-version 2
-# wsl --install -d Ubuntu-22.04
-# wsl --set-default Ubuntu-22.04
-# wsl --set-version Ubuntu-22.04 2
+# Download and install Ubuntu 22.04 appx package
+$distroName = "Ubuntu-22.04"
+$downloadUrl = "https://aka.ms/wslubuntu2204"
+$tempFile = "$env:TEMP\Ubuntu-22.04.appx"
+
+Write-Host "Downloading Ubuntu 22.04 appx package from $downloadUrl..."
+try {
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $tempFile -ErrorAction Stop
+} catch {
+    Write-Error "Failed to download Ubuntu 22.04 appx package: $_"
+    exit 1
+}
+
+Write-Host "Installing Ubuntu 22.04 appx package..."
+try {
+    Add-AppxPackage -Path $tempFile -ErrorAction Stop
+} catch {
+    Write-Error "Failed to install Ubuntu 22.04 appx package: $_"
+    exit 1
+}
+
+Write-Host "Installation complete. To launch Ubuntu 22.04, run the command 'wsl --distribution $distroName'."
+
 # sudo apt update && sudo apt full-upgrade
+
+# Enable WSLg (requires newer versions of window 10 or 11)
 # sudo nano /etc/wsl.conf
 # [boot]
 # systemd=true
@@ -71,11 +99,14 @@ try {
 # Install GUI for WSL2
 # sudo apt update && sudo apt install x11-apps
 # xeyes &
-# xcalc // Run calculator gui
+# xcalc // Run calculator GUI
 
 # Install Docker on WSL2
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
+# sudo apt install --no-install-recommends apt-transport-https ca-certificates curl gnupg2
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
+# sudo apt update
+# sudo apt install docker-ce docker-ce-cli containerd.io
 # sudo usermod -aG docker $USER
 # sudo apt-get update && sudo apt-get install docker-compose-plugin
 # docker --version
@@ -88,3 +119,10 @@ try {
 #             --exec /usr/sbin/service docker start > /dev/null 2>&1
 #     fi
 # fi
+
+# Choco Install of ddev in wsl (requires wsl to be setup)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ddev/ddev/master/scripts/install_ddev_wsl2_docker_inside.ps1'))
+
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ddev/ddev/master/scripts/install_ddev_wsl2_docker_desktop.ps1'))

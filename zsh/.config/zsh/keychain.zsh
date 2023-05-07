2b219fe2578c61e52c9bd9b827a8cb22f37934b9
Joel Maximilian Mai <joel@maispace.de>
2023-05-07 15:39:03 +0200

    [TASK] Fix zsh keychain setup

--- zsh/.config/zsh/keychain.zsh
+++ zsh/.config/zsh/keychain.zsh
@@ -1,7 +1,7 @@
 # Adjusts the agents that keychain manages:
-zstyle :omz:plugins:keychain agents ssh
+zstyle :omz:plugins:keychain agents gpg,ssh
+zstyle :omz:plugins:keychain identities iwmedien iwmedien-nb-705
 # Add sshkey on startup but wait for use 
 # (remove --noask for instant prompt and ask only if id is needed)
-/usr/bin/keychain $HOME/.ssh/iwmedien
-source $HOME/.keychain/$HOSTNAME-sh
+source $HOME/.keychain/NB-705-sh
 eval $(keychain -q --noask --eval iwmedien)
